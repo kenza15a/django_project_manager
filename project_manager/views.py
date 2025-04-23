1f185a2530c6958d9735d01fb60355bbb25a2a15
Kenza Filali <filali1991.univ@gmail.com>
2025-04-23 10:59:57 +0200

    swagger docs with tags and deails

--- project_manager/views.py
+++ project_manager/views.py
@@ -13,6 +13,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth import get_user_model
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 
 class StandardPagination(PageNumberPagination):
     page_size = 5
@@ -29,6 +32,44 @@ class UserViewSet(viewsets.ModelViewSet):
     search_fields = ['username', 'email']
     ordering_fields = ['username', 'email']
 
+    @swagger_auto_schema(
+        operation_summary="Create a new user",
+        request_body=UserSerializer,
+        responses={201: UserSerializer}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Retrieve a user by ID",
+        responses={200: UserSerializer}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Update user data completely",
+        request_body=UserSerializer,
+        responses={200: UserSerializer}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Partially update user data",
+        request_body=UserSerializer,
+        responses={200: UserSerializer}
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Delete a user",
+        responses={204: 'No Content'}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -37,31 +78,84 @@ class ProjectViewSet(viewsets.ModelViewSet):
     pagination_class = StandardPagination
     filter_backends = [filters.OrderingFilter,
                        filters.SearchFilter, DjangoFilterBackend]
-    
+
     search_fields = ['title']
     ordering_fields = ['title', 'created_at']
-    filterset_fields = ['owner','title']
+    filterset_fields = ['owner', 'title']
+
+    @swagger_auto_schema(
+        operation_summary="List all projects",
+        operation_description="""
+        Returns a paginated list of all projects.
+        Supports filtering by owner and title,
+        searching by title, and ordering by title or creation date.
+        """,
+        responses={200: ProjectSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Create a new project",
+        operation_description="Only authenticated users can create projects.",
+        request_body=ProjectSerializer,
+        responses={201: ProjectSerializer}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Retrieve project details",
+        responses={200: ProjectSerializer, 404: "Not Found"}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Fully update a project",
+        request_body=ProjectSerializer,
+        responses={200: ProjectSerializer}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Partially update a project",
+        request_body=ProjectSerializer,
+        responses={200: ProjectSerializer}
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Delete a project",
+        responses={204: "No Content"}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         user = self.request.user
 
         # S'assurer que l'utilisateur est connecté
         if not user or not user.is_authenticated:
-            raise PermissionDenied("Authentication required to create a project.")
+            raise PermissionDenied(
+                "Authentication required to create a project.")
 
-        # Si c'est un SimpleLazyObject, le convertir
         if not isinstance(user, User):
             user = User.objects.get(pk=user.pk)
 
-        # Créer le projet
         serializer.save(owner=user)
-
 
 # vues personnalisées
 # Vue pour /api/users/register/
 
 
 class RegisterUserView(APIView):
+    @swagger_auto_schema(
+        request_body=UserSerializer,
+        responses={201: UserSerializer, 400: "Bad Request"}
+    )
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -98,10 +192,16 @@ class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     lookup_field = 'id'
 
+    @swagger_auto_schema(
+        responses={200: ProjectSerializer, 404: 'Not Found'}
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
     def get_object(self):
         obj = get_object_or_404(
             Project,
-            id=self.kwargs['id'], 
+            id=self.kwargs['id'],
             owner__username=self.kwargs['username']
         )
         self.check_object_permissions(self.request, obj)
